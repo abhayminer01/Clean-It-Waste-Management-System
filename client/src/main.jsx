6b0cbdeff2b1756a3904dd4ae61e0b243778f117
abhayminer01 <abhayvapvl@gmail.com>
2025-09-19 20:46:31 +0530

    frontend setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home-page';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = { <HomePage /> } />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
