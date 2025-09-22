5e8b6cfc7e48bc01765c5146384d0f61fbaead32
abhayminer01 <abhayvapvl@gmail.com>
2025-09-22 20:26:25 +0530

    setup again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,8 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/home-page';
-import LoginPage from './pages/login-page';
-import LaunchPage from './pages/launch-page';
-import RegisterPage from './pages/register-page';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element = { <LaunchPage /> } />
-        <Route path='home' element = { <HomePage /> } />
-        <Route path='login' element = { <LoginPage /> } />
-        <Route path='register' element = { <RegisterPage /> } />
-      </Routes>
-    </BrowserRouter>
   </StrictMode>
 )
---
+++ client/src/pages/LaunchPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LaunchPage() {
+  return (
+    <div>LaunchPage</div>
+  )
+}
