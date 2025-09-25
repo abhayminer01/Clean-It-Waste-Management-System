24bba86aa4612bb63af59ba8ffbea53d1f58ea3a
abhayminer01 <abhayvapvl@gmail.com>
2025-09-25 23:54:32 +0530

    auth api labelling

--- client/src/services/auth-api.js
+++ client/src/services/auth-api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const BASE_URL = "http://localhost:5000/api"
 
+// USER LOGIN
 export const userLogin = async (payload) => {
     try {
         const req = await axios.post(`${BASE_URL}/auth/login`, payload, {
@@ -14,7 +15,7 @@ export const userLogin = async (payload) => {
     }
 }
 
-
+// USER REGISTRATION
 export const userRegistration = async (payload) => {
     try {
         const req = await axios.post(`${BASE_URL}/auth/register`, payload, {
